4d1600574a2ed5e8c84f83dfdb7992e073f01af1
MarekRousar <rousar.marek1@gmail.com>
2026-05-07 09:34:23 +0200

    Update Program.cs

--- Moje kody/Bankovní účet (OOP2)/Program.cs
+++ Moje kody/Bankovní účet (OOP2)/Program.cs
@@ -1,1 +1,40 @@
+public class Ucet
+{
+    public double Zustatek { get; set; }
+    public int Zalozeni { get; set; }
 
+    public Ucet(double zustatek, int zalozeni)
+    {
+        Zustatek = zustatek;
+        Zalozeni = zalozeni;
+    }
+
+    public void ZobrazZustatek()
+    {
+        Console.WriteLine($"Zustatek na tvem uctu je: {Zustatek} Kc");
+    }
+
+    public void ZobrazZalozeni()
+    {
+        Console.WriteLine($"Ucet jsi zalozil pred {Zalozeni} lety");
+    }
+
+    public void Vlozit(double vklad)
+    {
+        Zustatek = (double)Zustatek + vklad;
+        Console.WriteLine($"Aktualne mas na uctu: {Zustatek} Kc");
+    }
+
+    public void Vyber(double vyber)
+    {
+        if (vyber > Zustatek)
+        {
+            Console.WriteLine($"Nemas dostatek penez na takovy vyber");
+        }
+        else
+        {
+            Zustatek = (double)Zustatek - vyber;
+            Console.WriteLine($"Aktualne mas na uctu: {Zustatek} Kc");
+        }
+    }
+}
